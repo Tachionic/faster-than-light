81eb409a05c20473bb95331578704ea66ef181a2
David Kneel <david.kneel@protonmail.com>
2021-06-04 21:03:40 +0100

    :art: Add UChild contracts to solcover ignore list.

--- .solcover.js
+++ .solcover.js
@@ -4,7 +4,9 @@ module.exports = {
     compileCommand: 'npm run compile',
     skipFiles: [
         'abdk-libraries-solidity',
-        'ERC20Mock.sol'
+        'ERC20Mock.sol',
+        'UChildAdministrableERC20.sol',
+        'UChildERC20Proxy.sol',
     ],
     providerOptions: {
         default_balance_ether: '10000000000000000000000000',
