8e6d4f476392c96552c3e7f6055beeba26e87c94
David Kneel <david.kneel@protonmail.com>
2021-06-04 20:58:06 +0100

    :fire: Remove unnecessary shell scripts.

--- scripts/copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-copy(){
-    local network=$1
-    # echo $network
-    cp AcceptedToken/deployments/$network/*.json deployments/$network/
-    cp -r AcceptedToken/artifacts artifacts
-}
